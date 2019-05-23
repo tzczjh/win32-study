03b6bd320a174787c06e6546fbf245b6303856ae
tzczjh <825672319@qq.com>
2019-05-24 00:30:15 +0800

    不想找图片 所以打算直接用代码画出来

---
+++ PushBox/MyPaint.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<Windows.h>
+
+/**
+	x, y为游戏横纵坐标， 每一格为40px 地图10*10
+**/
+
+void DrawWall(LPPAINTSTRUCT lpPS, int x, int y) {
+
+}
+
+void DrawBox(LPPAINTSTRUCT lpPS, int x, int y) {
+
+}
+
+void DrawPlayer(LPPAINTSTRUCT lpPS, int x, int y) {
+
+}
